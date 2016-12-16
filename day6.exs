48d75d0ed5ec6608161eb9430230bb4574e696a4
Tablesaw Tablesawsen <tablesaw@gmail.com>
2016-12-15 19:24:00 -0800

    DRY Day 6

--- day6.exs
+++ day6.exs
@@ -1,38 +1,37 @@
 defmodule Day6 do
     def run do
-        File.read!('day6.txt')
-        |> String.split
-        |> Enum.map(&String.graphemes/1)
-        |> List.zip
-        |> Enum.map(&Tuple.to_list/1)
+        input_by_columns
         |> Enum.map(&most_common_letter/1)
         |> Enum.join
     end
 
-    def most_common_letter(list) do
-        list
-        |> Enum.sort
-        |> Enum.chunk_by(&(&1))
-        |> Enum.sort_by(&length/1, &>=/2)
-        |> List.first
-        |> List.first
+    def run_2 do
+        input_by_columns
+        |> Enum.map(&least_common_letter/1)
+        |> Enum.join
     end
 
-    def run_2 do
+    defp input_by_columns do
         File.read!('day6.txt')
         |> String.split
         |> Enum.map(&String.graphemes/1)
         |> List.zip
         |> Enum.map(&Tuple.to_list/1)
-        |> Enum.map(&least_common_letter/1)
-        |> Enum.join
     end
 
     def least_common_letter(list) do
+        _common_letter list, &<=/2
+    end
+
+    def most_common_letter(list) do
+        _common_letter list, &>=/2
+    end
+
+    defp _common_letter(list, sort) do
         list
         |> Enum.sort
         |> Enum.chunk_by(&(&1))
-        |> Enum.sort_by(&length/1, &<=/2)
+        |> Enum.sort_by(&length/1, sort)
         |> List.first
         |> List.first
     end

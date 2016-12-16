91035d9e23415a409100500df5f0b473f7c97278
Tablesaw Tablesawsen <tablesaw@gmail.com>
2016-12-15 19:19:17 -0800

    Day6b

--- day6.exs
+++ day6.exs
@@ -17,4 +17,23 @@ defmodule Day6 do
         |> List.first
         |> List.first
     end
+
+    def run_2 do
+        File.read!('day6.txt')
+        |> String.split
+        |> Enum.map(&String.graphemes/1)
+        |> List.zip
+        |> Enum.map(&Tuple.to_list/1)
+        |> Enum.map(&least_common_letter/1)
+        |> Enum.join
+    end
+
+    def least_common_letter(list) do
+        list
+        |> Enum.sort
+        |> Enum.chunk_by(&(&1))
+        |> Enum.sort_by(&length/1, &<=/2)
+        |> List.first
+        |> List.first
+    end
 end

cbcb24aac0e9d8bf03bf1696c98c11766cd6d6ef
Tablesaw Tablesawsen <tablesaw@gmail.com>
2017-10-11 20:16:13 -0700

    Add Astar to day 11

---
+++ lib/day11.ex
@@ -0,0 +1,116 @@
+require Astar
+
+defmodule Day11 do
+    def test do
+        env = {&RTF.possible_moves/1, &RTF.edge_cost/2, &RTF.heuristic_to_state/2}
+        path = Astar.astar env, RTF.test, RTF.goal(RTF.test)
+        length(path)
+    end
+
+    def part_one do
+        env = {&RTF.possible_moves/1, &RTF.edge_cost/2, &RTF.heuristic_to_state/2}
+        path = Astar.astar env, RTF.real, RTF.goal(RTF.real)
+        length(path)
+    end
+
+end
+
+defmodule RTF do
+
+    def test do
+        f1 = MapSet.new [{:H, :chip}, {:Li, :chip}]
+        f2 = MapSet.new [{:H, :generator}]
+        f3 = MapSet.new [{:Li, :generator}]
+        f4 = %MapSet{}
+        %{  floors: %{1 => f1, 2 => f2, 3 => f3, 4 => f4},
+            elevator: 1
+        }
+    end
+
+    def goal %{floors: floors} do
+        f1 = %MapSet{}
+        f2 = %MapSet{}
+        f3 = %MapSet{}
+        f4 = for {_, floor} <- floors, item <- floor, into: %MapSet{}, do: item
+        %{  floors: %{1 => f1, 2 => f2, 3 => f3, 4 => f4},
+            elevator: 4
+        }
+    end
+
+    def real do
+        f1 = MapSet.new [{:Tm, :generator}, {:Tm, :chip}, {:Pu, :generator}, {:Sr, :generator}]
+        f2 = MapSet.new [{:Pu, :chip}, {:Sr, :chip}]
+        f3 = MapSet.new [{:Pm, :generator}, {:Pm, :chip}, {:Ru, :generator}, {:Ru, :chip}]
+        f4 = %MapSet{}
+        %{  floors: %{1 => f1, 2 => f2, 3 => f3, 4 => f4},
+            elevator: 1
+        }
+    end
+
+    def directory %{floors: floors, elevator: elevator_location} do
+        directory = %{elevator: elevator_location}
+        for {level, floor} <- floors, item <- floor, into: directory, do: {item, level}
+    end
+
+    def edge_cost(_, _), do: 1
+
+    def combination(0, _), do: [[]]
+    def combination(_, []), do: []
+    def combination(n, [x|xs]) do
+      (for y <- combination(n - 1, xs), do: [x|y]) ++ combination(n, xs)
+    end
+
+    def possible_elevators %{elevator: floor} = rtf do
+         list = MapSet.to_list rtf.floors[floor]
+         combination(2, list) ++ combination(1, list)
+    end
+
+    def possible_moves rtf do
+        elevators = possible_elevators rtf
+
+        up = if Map.has_key?(rtf.floors, rtf.elevator + 1) do
+                Enum.map(elevators, &(move_elevator rtf, &1, rtf.elevator + 1))
+             else
+                []
+             end
+        dn = if Map.has_key?(rtf.floors, rtf.elevator - 1) do
+                Enum.map(elevators, &(move_elevator rtf, &1, rtf.elevator - 1))
+             else
+                []
+             end
+
+        Enum.filter (up ++ dn), &RTF.is_valid?/1
+    end
+
+    def move_elevator rtf, el_inv, floor do
+        new_rtf = Enum.reduce el_inv, rtf, &(move_item &2, &1, floor)
+        %{new_rtf | elevator: floor}
+    end
+
+    def is_valid? rtf do
+        Map.values(rtf.floors)
+        |>  Enum.all?(fn floor ->
+                Enum.all? floor, &(item_is_valid? &1, floor)
+            end)
+    end
+
+    def item_is_valid? {el, type}, floor do
+        type == :generator ||
+            not Enum.any?(floor, fn {_, type} -> type == :generator end) ||
+            MapSet.member? floor, {el, :generator}
+    end
+
+
+    def move_item rtf, item, floor do
+        rtf
+        |> update_in([:floors, rtf.elevator], &(MapSet.delete &1, item))
+        |> update_in([:floors, floor], &(MapSet.put &1, item))
+    end
+
+    def heuristic_to_state rtf1, rtf2 do
+        directory1 = RTF.directory rtf1
+        directory2 = RTF.directory rtf2
+        Enum.reduce(directory1, 0, fn {item, flr}, acc -> acc + abs(flr - directory2[item]) end)
+    end
+
+end
